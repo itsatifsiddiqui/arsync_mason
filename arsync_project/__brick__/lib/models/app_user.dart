4bf2d0625c875605b174292b95654e293a2bc84c
Atif Siddiqui <itsatifsiddiqui@gmail.com>
2025-05-26 13:35:28 +0500

    fix appuser issue.

--- arsync_project/__brick__/lib/models/app_user.dart
+++ arsync_project/__brick__/lib/models/app_user.dart
@@ -4,7 +4,7 @@ part 'app_user.freezed.dart';
 part 'app_user.g.dart';
 
 @freezed
-class AppUser with _$AppUser {
+abstract class AppUser with _$AppUser {
   const factory AppUser({
     String? userid,
     required String name,
